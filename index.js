5181c619c466e8c17ac27acc54f99083a5fe3358
=vangie <=evegabayan16@gmail.com>
2020-11-25 12:35:51 +0800

    Partials w/ Controllers

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const bodyParser = require('body-parser');
 const database = require("./services/database");
 const loginRouter = require ("./router/login");
-// const directRouter = require("./router/indexRouter");
-const { title } = require("process");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use('/static', express.static(path.join(__dirname, 'source')))
+app.use('/static', express.static('source'))
 app.set('view engine', 'ejs');
-// app.use(cookieParser('secret'));
 database.connect();
 
-<<<<<<< HEAD
-
-=======
-app.use("/home", directRouter);
->>>>>>> 426d9e1faabb914d7c04d6f11b8d69480e9de4ae
 app.use("/",loginRouter);
 
 const directRouter = require("./router/indexRouter");
--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,14 +1,15 @@
-const directController = require('../controllers/indexController');
+const directController = require('../controllers/directController');
 const express = require('express');
 const router = express.Router();
 
 
 router.get('/home', directController.gotoHome);
-router.get('/appetizers', directController.gotoAppetizers);
-router.get('/main_course', directController.gotoMain);
-router.get('/desserts', directController.gotoDesserts);
+router.get('/recipes/:category', directController.gotoCategory);
 router.get('/features', directController.gotoFeatures);
 router.get('/about', directController.gotoAbout);
+router.get('/create/recipes',directController.createForm)
+router.get('/recipes/:category/:id',directController.show)
+router.post('/recipes/create',directController.store)
 
 module.exports = router;
 
